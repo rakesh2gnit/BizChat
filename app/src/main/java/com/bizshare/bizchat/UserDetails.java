2f18a82a33155e46cb6e609c8c860fd2ccfdaa92
Rakesh kumar <Rakesh.Kumar@corpusinc.corp>
2018-03-16 18:13:01 +0530

    add all chat screen

---
+++ app/src/main/java/com/bizshare/bizchat/UserDetails.java
@@ -0,0 +1,12 @@
+package com.bizshare.bizchat;
+
+/**
+ * Created by Rakesh.Kumar on 16-03-2018.
+ */
+
+public class UserDetails {
+
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
